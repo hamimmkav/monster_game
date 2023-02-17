a9b46c7787578048de5abef89930bfbe12a191e3
Hamim Kav <hamimmkav@gmail.com>
2023-02-18 01:25:00 +0300

    add button functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,16 +10,27 @@ new Vue({
        this.game_is_on = true;
      },
      attach: function(){
+       var point = Math.ceil(Math.random() * 10);
+       this.monster_heal -= point;
+       this.monster_attach();
 
      },
      speacial_attack: function(){
-
+       var point = Math.ceil(Math.random() * 25);
+       this.monster_heal -= point;
+       this.monster_attach();
+     },
+     monster_attach: function(){
+       var point = Math.ceil(Math.random() * 15);
+       this.player_heal -= point;
      },
      heal_up: function(){
-
+       var point = Math.ceil(Math.random() * 20);
+       this.player_heal += point;
+       this.monster_attach();
      },
      give_up: function(){
-
+       this.player_heal = 0;
      },
   }
 })
